5b046c50fff6c4a41bedd5cfbd3f82f30b2fc3c5
Pedro Vinícius <166882672+vinipedro629@users.noreply.github.com>
2025-10-18 16:11:12 +0000

    11 commit

--- meu-projeto/src/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-export class Controller {
-    async getAll(req, res) {
-        // Lógica para obter todos os itens
-    }
-
-    async getById(req, res) {
-        // Lógica para obter um item pelo ID
-    }
-}
